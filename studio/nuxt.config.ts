865aafc769715ee68c20e7c59627bd4381b6d531
zty <zhangtianyu@seedleap.ai>
2025-11-13 15:32:29 +0800

    init studio

---
+++ studio/nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@pinia/nuxt'],
+  css: [],
+  postcss: {
+    plugins: {
+      '@tailwindcss/postcss': {},
+      autoprefixer: {},
+    },
+  },
+  typescript: {
+    strict: true,
+  },
+})
